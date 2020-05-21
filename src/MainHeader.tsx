a25910e833d33e818fe4ea1eeca03c56fb07d6d3
Gordana Minovska <gordanam@fileonq.com>
2020-05-20 18:56:44 -0700

    Create MainHeader component

---
+++ src/MainHeader.tsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
+
+import AppBar from "@material-ui/core/AppBar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import Typography from "@material-ui/core/Typography";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+    },
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    drawerContainer: {
+      overflow: "auto",
+    },
+    content: {
+      marginTop: "60px",
+      flexGrow: 1,
+      padding: theme.spacing(3),
+    },
+  })
+);
+
+export default function Header() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h4" noWrap>
+            CovidHub
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      {/* <Drawer
+          className={classes.drawer}
+          variant="permanent"
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+        >
+          <Toolbar />
+          <div className={classes.drawerContainer}></div>
+        </Drawer> */}
+    </div>
+  );
+}
