861384b9921800a0bf7d5808cda6bcf2c07fb027
Gordana Minovska <gordanam@fileonq.com>
2020-05-23 19:59:17 -0700

    Refactor - parse accept-headers to get language preference

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,44 @@
 import React from "react";
 import dynamic from "next/dynamic";
+import { GetServerSideProps } from "next";
+import parser from "accept-language-parser";
+import { IntlProvider } from "react-intl";
+import Header from "../src/MainHeader";
+import MainContent from "../src/MainContent";
 
-const Dashboard = dynamic(() => import("../src/Dashboard"), { ssr: false }); // because it uses navigator object
+import enUS from "../translations/en-US.json";
+import sr from "../translations/sr-latn-RS.json";
+import de from "../translations/de-DE.json";
 
-export default function Index() {
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const userLanguage = parser.pick(
+    ["en", "sr", "de"],
+    context.req.headers["accept-language"]
+  );
+
+  return {
+    props: {
+      lang: userLanguage || "en-US",
+    }, // will be passed to the page component as props
+  };
+};
+
+const message = {
+  ["en"]: enUS,
+  ["sr"]: sr,
+  ["de"]: de,
+};
+
+export default function Index(props) {
+  console.log(props);
   return (
-    <>
-      <Dashboard />
-    </>
+    <IntlProvider
+      locale={props.lang}
+      defaultLocale="en-US"
+      messages={message[props.lang]}
+    >
+      <Header />
+      <MainContent lang={props.lang} />
+    </IntlProvider>
   );
 }
