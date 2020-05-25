f6b3bd4db526558f8725295e7f84e034cf622d26
Gordana Minovska <gordanam@fileonq.com>
2020-05-25 16:41:36 -0700

    Add RelativeTime component

---
+++ src/RelativeTime.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import TimeAgo, { TimeAgoProps } from "timeago-react";
+import * as timeago from "timeago.js";
+import sr from "timeago.js/lib/lang/sr";
+timeago.register("sr", sr);
+
+const RelativeTime = (props: TimeAgoProps) => {
+  return <TimeAgo {...props} />;
+};
+
+export default RelativeTime;
